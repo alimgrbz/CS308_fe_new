572943a0ad141bd6a552754079885e49773234da
Alım Gürbüz <alimgurbuz@sabanciuniv.edu>
2025-03-17 20:31:42 +0300

    new

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Logo.css';
@@ -7,8 +6,8 @@ const Logo = ({ size = 'medium' }) => {
   return (
     <Link to="/" className={`logo logo-${size}`}>
       <img 
-        src="/lovable-uploads/7a59c123-35a9-4ddd-b11c-fe5ca8e73419.png" 
-        alt="Driftmood Coffee" 
+        src="/logo.png" 
+        alt="DriftMood Coffee" 
         className="logo-image"
       />
     </Link>
